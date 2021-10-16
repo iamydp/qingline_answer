cadc00c364aa617021f1298323d62c66c754f8a4
iamydp <iamydp@hotmail.com>
2021-10-16 20:48:10 +0800

    Update 计算将水从初始温度加热到最终温度所需的能量.cpp

--- 计算将水从初始温度加热到最终温度所需的能量.cpp
+++ 计算将水从初始温度加热到最终温度所需的能量.cpp
@@ -7,3 +7,14 @@ int main()
 	q = m * (f - i) * 4184;
 	printf("%.2f焦耳", q);    //.2表示输出保留两位小数
 }
+
+//以上是我的答案,qingline官方答案如下,批注为"注意int型和double型数据之间的运算规则"
+//#include "stdio.h"
+//int main()
+//{
+//	int m,t1,t2;
+//	double q;
+//	scanf("%d %d %d",&m,&t1,&t2); 
+//	q=4184.0*m*(t2-t1);
+//	printf("%.2f焦耳\n",q);
+//}

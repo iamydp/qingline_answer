a6d6a1f9cd695130cab24748fae92ef79f2fe0e0
iamydp <iamydp@hotmail.com>
2021-10-19 22:47:28 +0800

    Create 算sin,cos,abs之类的.cpp

---
+++ 算sin,cos,abs之类的.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	float x, y;
+	int a, b;
+	scanf_s("%f%f", &x, &y);
+	printf_s("sin(x):%lf\n", sin(x * 3.1415 / 180));  //操你妈qingline里面的1为角度值,是个人都会认为是弧度制吧
+	printf_s("cos(x):%lf\n", cos(x * 3.1415 / 180));
+	printf_s("|x|:%f\n", c);
+	printf_s("e的x次方:%f\n", exp(x));
+	printf_s("x的y次方:%f", pow(x, y));
+}

0bbcc09eac33a61501d99a04d1fd7acaef7a4821
iamydp <iamydp@hotmail.com>
2021-10-19 22:53:47 +0800

    Update 算sin,cos,abs之类的.cpp

--- 算sin,cos,abs之类的.cpp
+++ 算sin,cos,abs之类的.cpp
@@ -2,9 +2,12 @@
 #include <math.h>
 int main()
 {
-	float x, y;
-	int a, b;
+	float x, y,c;
 	scanf_s("%f%f", &x, &y);
+	int a, b;
+	a = x;
+	b = abs(a);
+	c = b;						  //本人才疏学浅(笑脸流汗
 	printf_s("sin(x):%lf\n", sin(x * 3.1415 / 180));  //操你妈qingline里面的1为角度值,是个人都会认为是弧度制吧
 	printf_s("cos(x):%lf\n", cos(x * 3.1415 / 180));
 	printf_s("|x|:%f\n", c);
